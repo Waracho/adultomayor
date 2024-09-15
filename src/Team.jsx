aabf5878e91d611db745b2154c86cf5675a67253
Carlos <cgmf.soul@gmail.com>
2024-09-14 23:01:45 -0300

    contactos y quienes somos

--- src/Team.jsx
+++ src/Team.jsx
@@ -1,104 +1,78 @@
 import React from 'react';
-import './Team.css';
 import bobbyjpg from './assets/Bobby.jpg';
+import cralosjpg from './assets/cralos.jpg';
+import camijpg from './assets/cami.jpg';
+import tebanjpg from './assets/teban.jpg';
+import anibaljpg from './assets/anibal.jpg';
+import pipejpg from './assets/pipe.jpg';
+import { Persona } from './Persona';
 
 const Team = () => {
 return (
-	<div className="flex flex-col flex-grow w-[75%]">
-		<div className="bg-yellow-500 text-4xl py-2">
-			El diseño gráfico es mi pasión
+	<div className="flex flex-col flex-grow w-[80%]">
+		<div className=" text-5xl py-5">
+			Conoce a nuestro equipo
 		</div>
 		<div className="flex flex-row">
-			<div className="bg-blue-500 w-full">
-			<p className="text-xl text-justify">
-				Buscamos aprobar nuestro proyecto de feria 2024, ahora estamos moviéndonos para encontrar un nuevo cliente que esté por fin dispuesta a contestarnos (opcional)
-			</p>
+			<div className="w-full">
+				<p className="text-xl text-justify pt-2">
+				Con la inspiración de feria de software y 
+				en busca de resolver los problemas de mitigación 
+				de polvo en minerías y constructoras del país, 
+				nuestro equipo de último año de la Universidad 
+				Federico Santa María forma&nbsp;
+				<span className="relative inline-block">
+					<span className="absolute inset-0 bg-yellow-500 -skew-y-3 z-0"></span>
+					<span className="relative text-white z-10">AviSoft</span>
+				</span>
+				, y nuestra aplicación&nbsp;
+				<span className="relative inline-block">
+					<span className="absolute inset-0 bg-yellow-500 -skew-y-3 z-0"></span>
+					<span className="relative text-white z-10">CanarIA</span>
+				</span>
+				.
+				</p>
 			</div>
 		</div>
-		<div className="container">
-			{/*Container de team*/}
-			<div className="box justify-center">
-				<div className='custom-center'>
-					<div className='bg-white rounded py-2 my-2 w-60'>
-						<p className='text-black'>Bobbyneitor</p>
-					</div>
-				</div>
-				<div className='image-container'>
-					<img src={bobbyjpg} alt="Este es el bobby :3" className='custom-image' />
-				</div>
-				<div className='custom-center'>
-					<div className='bg-white my-2 py-2 rounded w-60'>
-						<p className='text-black '>Redes Sociales</p>
-					</div>
-				</div>
-			</div>
-			{/*Container de team*/}
-			<div className="box justify-center">
-				<div className='custom-center'>
-					<div className='bg-white rounded py-2 my-2 w-60'>
-						<p className='text-black'>Bobbyneitor</p>
-					</div>
-				</div>
-				<div className='image-container'>
-					<img src={bobbyjpg} alt="Este es el bobby :3" className='custom-image' />
-				</div>
-				<div className='custom-center'>
-					<div className='bg-white my-2 py-2 rounded w-60'>
-						<p className='text-black '>Redes Sociales</p>
-					</div>
-				</div>
-			</div>
-			{/*Container de team*/}
-			<div className="box justify-center">
-				<div className='custom-center'>
-					<div className='bg-white rounded py-2 my-2 w-60'>
-						<p className='text-black'>Bobbyneitor</p>
-					</div>
-				</div>
-				<div className='image-container'>
-					<img src={bobbyjpg} alt="Este es el bobby :3" className='custom-image' />
-				</div>
-				<div className='custom-center'>
-					<div className='bg-white my-2 py-2 rounded w-60'>
-						<p className='text-black '>Redes Sociales</p>
-					</div>
-				</div>
-			</div>
-
-
-				{/*Container de team*/}
-				<div className="box justify-center">
-					<div className='custom-center'>
-						<div className='bg-white rounded py-2 my-2 w-60'>
-							<p className='text-black'>Bobbyneitor</p>
-						</div>
-					</div>
-					<div className='image-container'>
-						<img src={bobbyjpg} alt="Este es el bobby :3" className='custom-image' />
-					</div>
-					<div className='custom-center'>
-						<div className='bg-white my-2 py-2 rounded w-60'>
-							<p className='text-black '>Redes Sociales</p>
-						</div>
-					</div>
-				</div>
-				{/*Container de team*/}
-				<div className="box justify-center">
-					<div className='custom-center'>
-						<div className='bg-white rounded py-2 my-2 w-60'>
-							<p className='text-black'>Bobbyneitor</p>
-						</div>
-					</div>
-					<div className='image-container'>
-						<img src={bobbyjpg} alt="Este es el bobby :3" className='custom-image' />
-					</div>
-					<div className='custom-center'>
-						<div className='bg-white my-2 py-2 rounded w-60'>
-							<p className='text-black '>Redes Sociales</p>
-						</div>
-					</div>
-				</div>
-			</div>
+		<div className="flex flex-wrap justify-center py-10">
+			<Persona
+				nombre="Felipe Buscaglia"
+				descripcion="Scrum Master - API Developer"
+				link_lk="https://www.linkedin.com/in/felipe-buscaglia-vasquez/"
+				link_mail="felipe.buscaglia@sansano.usm.cl"
+				fotoPersona={pipejpg}
+			/>
+			<Persona
+				nombre="Carlos Mundaca"
+				descripcion="Product Owner - FullStack"
+				link_lk="https://www.linkedin.com/in/cgmf/"
+				link_mail="cgmf.soul@gmail.com"
+				link_IG="https://www.instagram.com/el_wacho_ese/?igsh=MWNjc2YyeGx0dDVvaA%3D%3D"
+				fotoPersona={cralosjpg}
+            />
+			<Persona
+				nombre="Camila Cid"
+				descripcion="IA Developer"
+				link_lk="https://www.linkedin.com/in/camila-cid-allendes-6b0443238/"
+				link_mail="camila.cida@sansano.usm.cl"
+				fotoPersona={camijpg}
+            />
+			<Persona
+				nombre="Anibal Plaza"
+				descripcion="IA Developer"
+				link_lk="https://www.linkedin.com/in/anibal-plaza-365088300/"
+				link_mail="anibal.plazar@usm.cl"
+				fotoPersona={anibaljpg}
+            />
+			<Persona
+				nombre="Esteban Carrasco"
+				descripcion="FullStack - AWS expert"
+				link_lk="https://www.linkedin.com/in/esteban-carrasco-galdame-3920a3216/"
+				link_mail="esteban.carrasco.ga@gmail.com"
+				fotoPersona={tebanjpg}
+            />
+			
+		</div>
 	</div>
 );
 }
